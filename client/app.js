e105120610f80ecffb8f35dd4c7b6a66c75508cf
ReneeHu <rhu@applevac.com>
2015-01-18 14:03:15 -0500

    angular routing and html

--- client/app.js
+++ client/app.js
@@ -1,6 +1,6 @@
-angular.module('Instagram', ['ngRoute', 'ngMessages'])
-  .config(function($routeProvider) {
-  	$routeProvider
+angular.module('Instagram', ['ngRoute', 'ngMessages', 'satellizer'])
+.config(function($routeProvider, $authProvider) {
+  $routeProvider
   .when('/', {
     templateUrl: 'views/home.html',
     controller: 'HomeCtrl'
@@ -18,4 +18,17 @@ angular.module('Instagram', ['ngRoute', 'ngMessages'])
     controller: 'DetailCtrl'
   })
   .otherwise('/');
+
+  $authProvider.loginUrl = 'http://localhost:3000/auth/login';
+  $authProvider.signupUrl = 'http://localhost:3000/auth/signup';
+  $authProvider.oauth2({
+    name: 'instagram',
+    url: 'http://localhost:3000/auth/instagram',
+    redirectUri: 'http://localhost:8000',
+    clientId: 'ffb44075745c4f3a83b7a033a15130fe',
+    requiredUrlParams: ['scope'],
+    scope: ['likes'],
+    scopeDelimiter: '+',
+    authorizationEndpoint: 'https://api.instagram.com/oauth/authorize'
   });
+});
